1db14968c30af2bad9bfd98a9191502503e86cac
Maryam Wasay <maryamwasay3@gmail.com>
2026-07-25 12:11:44 -0700

    Create public.py

---
+++ app/routers/public.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/public",
+    tags=["Public"]
+)
+
+@router.get("/info")
+def public_info():
+    return {
+        "message": "Welcome stranger! This info is public."
+    }
